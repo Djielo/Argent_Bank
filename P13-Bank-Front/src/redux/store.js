1cb5aac7e669061b075bb5128e1049029b0601c1
Djielo <business.jlo68@gmail.com>
2022-08-25 10:52:31 +0200

    refactor: Displacement of the reducers
    Move reducers to reducers folder

--- P13-Bank-Front/src/redux/store.js
+++ P13-Bank-Front/src/redux/store.js
@@ -1,8 +1,8 @@
 import { legacy_createStore as createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import userReducer from "./userReducer";
-import globalReducer from "./globalReducer";
+import userReducer from "../reducers/userReducer";
+import globalReducer from "../reducers/globalReducer";
 
 const store = createStore(combineReducers({
   user: userReducer,
