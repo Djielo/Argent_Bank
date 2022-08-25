395b681921584747e9008683fd7fe194c8570eec
Djielo <business.jlo68@gmail.com>
2022-08-25 11:04:12 +0200

    feat: Create a getProfileData action
    This action allows you to define if the conditions are met to connect the user and thus display his data

---
+++ P13-Bank-Front/src/actions/getProfileData.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+function getProfileData() {
+  axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem('token');
+  return async (dispatch) => {
+    try{
+      const response = await axios.get('/api/profile');
+      dispatch({ type: 'GET_PROFILE_DATA', payload: response.data });
+    }
+    catch(error){
+      dispatch({ type: 'SIGN_IN' });
+      localStorage.clear();
+      sessionStorage.clear();
+      dispatch({ type: 'GET_PROFILE_DATA', payload: { firstName: "", lastName: "" } });
+    }
+    
+  };
+}
+
+
+export {
+  getProfileData,
+}
