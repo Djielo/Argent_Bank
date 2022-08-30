1cfffff1b34f44cefba7412137dde19ffccaa8bb
Djielo <business.jlo68@gmail.com>
2022-08-30 12:05:07 +0200

    feat: Display management according to signIn or signOut

--- P13-Bank-Front/src/components/Header/Header.jsx
+++ P13-Bank-Front/src/components/Header/Header.jsx
@@ -1,18 +1,40 @@
 import logo from "../../../src/assets/argentBankLogo.png";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { logout } from "../../../src/services/auth.service";
 
-const Header = () => {
+const Header = ({userData}) => {
+  let storageState = localStorage.getItem("user") || sessionStorage.getItem("user");
+  const location = useLocation().pathname;
+  useEffect(() => {}, [location]);
+
   return (
     <>
       <nav className="main-nav">
-        <a className="main-nav-logo" href="/">
+        <Link className="main-nav-logo" to={{ pathname: "/" }}>
           <img className="main-nav-logo-image" src={logo} alt="Argent Bank Logo" />
           <h1 className="sr-only">Argent Bank</h1>
-        </a>
+        </Link>
         <div>
-          <a className="main-nav-item" href="/user/signIn">
-            <i className="main-nav-icon fa fa-user-circle"></i>
-            <div className="main-nav-text">Sign In</div>
-          </a>
+          {storageState === null ? (
+            <Link className="main-nav-item" to={{ pathname: "/user/login" }}>
+              <i className="main-nav-icon fa fa-user-circle"></i>
+              <div className="main-nav-text">Sign In</div>
+            </Link>
+          ) : (
+            <Link
+              className="main-nav-item"
+              to={{ pathname: "/" }}
+              onClick={() => {
+                logout();
+              }}
+            >
+              <i className="main-nav-icon fa fa-user-circle"></i>
+              <p>{userData.firstName}</p>
+              <i className="main-nav-icon fa fa-sign-out"></i>
+              <div className="main-nav-text">Sign Out</div>
+            </Link>
+          )}
         </div>
       </nav>
     </>
