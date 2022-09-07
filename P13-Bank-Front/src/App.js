706146fff8bf04131b48c2559a0883bd5750d8ec
Djielo <business.jlo68@gmail.com>
2022-09-07 15:27:24 +0200

    feat: Create store.js - Import store and Provider in App.js

--- P13-Bank-Front/src/App.js
+++ P13-Bank-Front/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import SignIn from "./pages/SignIn/SignIn";
@@ -6,21 +6,24 @@ import User from "./pages/Profil/Profil";
 import Error from "./pages/Error/Error";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
-  const [userData, setUserData] = useState();
 
   return (
-    <BrowserRouter>
-      <Header userData={userData}/>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/user/login" element={<SignIn setUserData={setUserData} />} />
-        <Route path="/user/profile" element={<User userData={userData} />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/user/login" element={<SignIn />} />
+          <Route path="/user/profile" element={<User />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ P13-Bank-Front/src/store.js
@@ -0,0 +1,6 @@
+import { legacy_createStore as createStore } from "redux";
+import userReducer from "./reducers/userReducer";
+
+const store = createStore(userReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export default store;
